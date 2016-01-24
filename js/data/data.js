9f689d29d624a40f33a8c69a02c4d6cac1c4aeee
kok liu <kokdemo@gmail.com>
2016-01-24 22:51:05 +0800

    add more info

--- js/data/data.js
+++ js/data/data.js
@@ -82,12 +82,125 @@ var jianghuInfo = {
                     {
                         name: '少林心法',
                         type: 'mind',
-                        describe: '少林寺基础的内功心法',
+                        describe: '少林虽然不是很重视内功，但基础内功心法也是十分著名的',
                         stage: [1, 0.6, 0.1, 0.05, 0],
                         base: 10,
                         proficiency: 0
                     }
+                ],
+                '般若堂弟子':[
+                    {
+                        name: '般若掌',
+                        type: 'force',
+                        describe: '般若堂中，集天下掌法为一体的传世掌法',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 100,
+                        proficiency: 0
+                    },
+                    {
+                        name: '修罗刀法',
+                        type: 'force',
+                        describe: '隐忍，隐忍',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 100,
+                        proficiency: 0
+                    },
+                    {
+                        name: '鹰爪功',
+                        type: 'force',
+                        describe: '以指为爪，练到极致可以开碑裂石',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 100,
+                        proficiency: 0
+                    }
+                ],
+                '达摩院执事':[
+                    {
+                        name: '拈花指',
+                        type: 'force',
+                        describe: '轻若拈花',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 500,
+                        proficiency: 0
+                    },
+                    {
+                        name: '韦陀棍法',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 500,
+                        proficiency: 0
+                    },
+                    {
+                        name: '如来千叶手',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 500,
+                        proficiency: 0
+                    }
+                ],
+                '戒律院首座':[
+                    {
+                        name: '燃木刀法',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 1000,
+                        proficiency: 0
+                    },
+                    {
+                        name: '伏魔杖法',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 1000,
+                        proficiency: 0
+                    },
+                    {
+                        name: '少林九阳功',
+                        type: 'mind',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 900,
+                        proficiency: 0
+                    },
+                    {
+                        name: '寂灭爪',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 1000,
+                        proficiency: 0
+                    }
+                ],
+                '少林寺方丈':[
+                    {
+                        name: '金刚伏魔圈',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 2000,
+                        proficiency: 0
+                    },
+                    {
+                        name: '大力金刚指',
+                        type: 'force',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 1000,
+                        proficiency: 0
+                    },
+                    {
+                        name: '易筋经',
+                        type: 'mind',
+                        describe: '我编不下去了……',
+                        stage: [1, 0.6, 0.1, 0.05, 0],
+                        base: 900,
+                        proficiency: 0
+                    }
                 ]
+
             },
             work: {
                 '沙弥': [
@@ -111,6 +224,32 @@ var jianghuInfo = {
                         name: '打扫藏经阁',
                         probability: 1,
                         print: '你认真的打扫藏经阁，无意当中读了不少佛经。',
+                        effect: function(){
+                            jianghu.calc({mind: 1,time: 1,kongfu:{name:'少林佛法',num:2}})
+                        }
+                    }
+                ],
+                '罗汉堂弟子': [
+                    {
+                        name: '打熬身体',
+                        probability: 0.4,
+                        print: '你认真训练，感觉自己的身体又上了一个新台阶。',
+                        effect: function(){
+                            jianghu.calc({body: 1,time: 1})
+                        }
+                    },
+                    {
+                        name: '切磋武艺',
+                        probability: 0.8,
+                        print: '你和师兄弟们切磋武艺。',
+                        effect: function(){
+                            jianghu.calc({force: 2,time: 1})
+                        }
+                    },
+                    {
+                        name: '研习佛法',
+                        probability: 1,
+                        print: '你认真的听讲佛法，感觉自己佛法精进了不少。',
                         effect: function(){
                             jianghu.calc({mind: 1,time: 1,kongfu:{name:'少林佛法',num:2}})
                         }
